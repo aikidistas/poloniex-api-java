7b46d5083833d77a01e16deeaa53f61e756bbba7
aikidistas <aikidistas@gmail.com>
2021-02-10 11:18:31 +0200

    retry is for generic api call, not on specific api call

--- src/main/java/api/rest/privateapi/read/completebalances/CompleteBalances.java
+++ src/main/java/api/rest/privateapi/read/completebalances/CompleteBalances.java
@@ -1,12 +1,12 @@
 package api.rest.privateapi.read.completebalances;
 
+import api.rest.ApiException;
 import api.rest.ApiReadException;
 import api.rest.Json;
 import api.rest.privateapi.read.completebalances.dto.CompleteBalanceDto;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.extern.log4j.Log4j2;
-import org.cactoos.scalar.Retry;
 
 import java.util.Map;
 
@@ -27,17 +27,15 @@ public class CompleteBalances implements CompleteBalancesData {
     @Override
     public Map<String, CompleteBalanceDto> data() throws ApiReadException {
         try {
-            return new Retry<Map<String, CompleteBalanceDto>>(
-                    () -> new Gson().fromJson(
-                            jsonSource.json(),
-                            new TypeToken<Map<String, CompleteBalanceDto>>() {
-                            }.getType()
-                    ),
-                    RETRY_ATTEMPTS
-            ).value();
-        } catch (Exception e) {
-            log.error("Can't read complete balances after multiple retries", e);
-            throw new ApiReadException("Can't read complete balances after multiple retries", e);
+            return new Gson().fromJson(
+                    jsonSource.json(),
+                    new TypeToken<Map<String, CompleteBalanceDto>>() {
+                    }.getType()
+            );
+        } catch (ApiException e) {
+            final String message = "Can't read complete balances";
+            log.error(message, e);
+            throw new ApiReadException(message, e);
         }
     }
 }
